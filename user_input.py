6a4bdc32ccee8c4c6aa63add5fe1149b315dee3d
Nishant Bisht <91425692+ashbishtb@users.noreply.github.com>
2022-04-11 17:29:00 +0530

    Create user_input.py

---
+++ user_input.py
@@ -0,0 +1,7 @@
+var  = int(input())            # this is how to take input as int
+#designing a simple calculator
+print("Enter two numbers : ")
+a = input()
+b = input()
+sum = int(a) + int(b)
+print("The sum of two numbers : ",sum)
