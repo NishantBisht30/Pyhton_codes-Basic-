0739a6b4b857717256b708e3a657b249657afd02
asus <nishantdon76@gmail.com>
2022-04-11 18:05:38 +0530

    new code

---
+++ operators.py
@@ -0,0 +1,41 @@
+'''
+print("5 ** 6 : ",5**6)          # exponential operator ..(it raises the  power ) for eg., here it means 5 ki power 6
+print("15 // 6 : ",15 // 6)          # gives integer answer ... // this is called floor division operator
+
+i = 5
+print(i <5)
+print(i >5)
+print(i ==5)
+print(i >=5)
+
+# and -> and operator ...... or -> or operator ..........
+'''
+
+#identity operator
+'''
+a= True
+b= False
+print( a is b)
+print(a is not b)
+
+'''
+
+#membership operator
+# in , not in
+'''
+list1 = [3,3,2,45,56,12,234,100]
+print(32 in list1)
+print(32  not in list1)
+print(2 in list1)
+'''
+
+#bitwise operator
+'''
+0->00
+1->01
+2 -> 10
+3->11
+'''
+# print(0 & 1)   # can use "and" also
+# print(0 | 1)     # can use "or" also
+# print(0 | 3)
