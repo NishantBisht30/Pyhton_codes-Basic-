0792bb96724ea354973df1fbbeec93046c97e028
Nishant Bisht <91425692+ashbishtb@users.noreply.github.com>
2022-04-11 10:14:52 +0530

    Create print.py

---
+++ print.py
@@ -0,0 +1,5 @@
+print("Hello World!")
+print("How are you?")                  # now this line will be printed in the next line
+
+print("Hello world!",end="")            #or you can do end = "," if u want a comma in-between
+print("How are you?")
