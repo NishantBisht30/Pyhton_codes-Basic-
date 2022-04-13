f4662aa18336a662fd8fef7e105dda47293674e3
Nishant Bisht <91425692+ashbishtb@users.noreply.github.com>
2022-04-13 10:26:53 +0530

    Create string_functions.py

---
+++ string_functions.py
@@ -0,0 +1,9 @@
+mystr = "Hello i'm here"
+
+print(len(mystr))  # gives length of string
+print(mystr.upper()) # lower to upper
+print(mystr.lower())    # upper to lower
+print(mystr[3:])     # string slicing
+print(mystr.count("z"))          # count the number of occurance
+print(mystr.isalnum())      # The isalnum() method returns True if all characters in the string are alphanumeric (either alphabets or numbers).
+print(mystr.replace("Hello","Ello"))  # replace the string
